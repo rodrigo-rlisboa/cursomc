e4ff536d859ac4512c5dca44750a0ba6c6324705
Rodrigo Lisboa <rodrigo.rlisboa@gmail.com>
2020-05-17 22:51:37 -0300

    Criação dos Dominios Pdido, Pagamento e Estado Pagamento

--- src/main/java/com/example/cursomc/domain/Cliente.java
+++ src/main/java/com/example/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.example.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable{
@@ -28,12 +29,16 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipoCliente;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 	}
@@ -103,6 +108,14 @@ public class Cliente implements Serializable{
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -127,5 +140,6 @@ public class Cliente implements Serializable{
 			return false;
 		return true;
 	}
+
 	
 }
---
+++ src/main/java/com/example/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.example.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.repositories.ClienteRepository;
+import com.example.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> categoria = repo.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+
+}
